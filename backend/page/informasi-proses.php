d6e61965bfc55a936cdc71d6e24258fccef4c207
budistwn15 <budi.setiawan112001@gmail.com>
2021-11-13 17:26:16 +0700

    informasi proses

--- backend/page/informasi-proses.php
+++ backend/page/informasi-proses.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST['delete'])) {
     $id_informasi = htmlentities(htmlspecialchars(strip_tags(trim($_POST['id_informasi']))));
-    $query = mysqli_query($koneksi, "DELETE FROM him_penduduk WHERE id_informasi='$id_informasi'");
+    $query = mysqli_query($koneksi, "DELETE FROM him_informasi WHERE id_informasi='$id_informasi'");
     if ($query) {
         $_SESSION['successDelete'] = "Data berhasil dihapus";
         header("Location: index?page=informasi");
